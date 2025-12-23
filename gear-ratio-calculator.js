190bae25c34d188c9caa1e6f14fc9d57be8df182
Professor_Cheese <annaji.garimella6@gmail.com>
2025-12-23 10:21:44 -0500

    Delete gear-ratio-calculator.js

--- gear-ratio-calculator.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("calculateRatio").addEventListener("click", function () {
-    const driver = Number(document.getElementById("driverTeeth").value);
-    const follower = Number(document.getElementById("followerTeeth").value);
-    console.log(driver, follower);
-});
